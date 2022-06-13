826f28ee59233db196b26aa84cd726a6bbb42648
meszkos <michalik515@gmail.com>
2022-06-13 11:19:44 +0200

    Completed Tutorial

--- DependencyInjection/ViewController.swift
+++ DependencyInjection/ViewController.swift
@@ -6,14 +6,34 @@
 //
 
 import UIKit
+import MyAppUIKit
+import APIKit
+
+extension APICaller: DataFetchable{}
+
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //hhtps://iosacademy.io/api/v1/courses/index.php
+        
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 250, height: 50))
+        view.addSubview(button)
+        
+        button.backgroundColor = .systemBlue
+        button.setTitle("Tap me", for: .normal)
+        button.center = view.center
+        button.setTitleColor(.white, for: .normal)
+        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
+        
     }
 
+    @objc private func didTapButton(){
+        let vc = CoursesViewController(dataFetchable: APICaller.shared)
+        present(vc, animated: true)
+    }
 
 }
 
---
+++ MyAppUIKit/MyAppUIKit.h
@@ -0,0 +1,18 @@
+//
+//  MyAppUIKit.h
+//  MyAppUIKit
+//
+//  Created by MacOS on 13/06/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyAppUIKit.
+FOUNDATION_EXPORT double MyAppUIKitVersionNumber;
+
+//! Project version string for MyAppUIKit.
+FOUNDATION_EXPORT const unsigned char MyAppUIKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyAppUIKit/PublicHeader.h>
+
+
